eee3ed5d7208db438a3264fffd7b5fc7dcd62862
samuel <samuel.benning@zento.fr>
2018-12-08 12:30:06 +0100

    starter

--- src/app/storage/storage.module.ts
+++ src/app/storage/storage.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StorageService } from './storage.service';
-import { StorageFacadePresenterComponent } from './storage-facade-presenter/storage-facade-presenter.component';
+import { StorageFacadePresenterComponent } from './ui/storage-facade-presenter/storage-facade-presenter.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { StorageFacadeContainerComponent } from './storage-facade-container/storage-facade-container.component';
+import { StorageFacadeContainerComponent } from './ui/storage-facade-container/storage-facade-container.component';
 import { StorageFlowFacade } from './z-flow/storage-flow.facade';
 
 @NgModule({
--- src/app/toast/toast.module.ts
+++ src/app/toast/toast.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastService } from './toast.service';
-import { ToastFacadePresenterComponent } from './toast-facade-presenter/toast-facade-presenter.component';
+import { ToastFacadePresenterComponent } from './ui/toast-facade-presenter/toast-facade-presenter.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ToastFacadeContainerComponent } from './toast-facade-container/toast-facade-container.component';
+import { ToastFacadeContainerComponent } from './ui/toast-facade-container/toast-facade-container.component';
 import { ToastFlowFacade } from './z-flow/toast-flow.facade';
 
 @NgModule({
